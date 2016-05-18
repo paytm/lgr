b6eea0df72c9b51c5200f0d2daa218a58d8cb1a1
Vidur Khanna <khannavidur93@gmail.com>
2016-05-18 17:34:16 +0530

    Optimizing stack trace capture
    * Now, no need to get stack trace every time ... use in template only if it is required by the format.
    
    * stack trace check moved away from setlogformat
    
    * not exposing captureStack any more

--- index.js
+++ index.js
@@ -9,11 +9,19 @@ var
     /* NPM Third Party */
     _                   = require('lodash'),
     NPMLOG              = require('npmlog'),
-    MOMENT              = require('moment');
+    MOMENT              = require('moment'),
     
     /* NPM Paytm */
     
     /* Project Files */
+
+    /* Global */
+    TEMPLATE_PRINTS     = [
+                            '__FUNC__',
+                            '__LINE__',
+                            '__FILE__',
+                            '__COLM__'
+                        ];
 
 function LGR(opts) {
     this.NPMLOG = NPMLOG;
@@ -68,61 +76,84 @@ function LGR(opts) {
         /* STDOUT will not get a copy of this erro rmessage */
     }.bind(this));
 
-    /*
-       Always nice to have __FUNC__, __FILE__, and __LINE__.
-       Referred from http://stackoverflow.com/questions/11386492/accessing-line-number-in-v8-javascript-chrome-node-js
-       Also read https://github.com/v8/v8/wiki/Stack-Trace-API
-    */
-
-    this.captureStack = function (){
-        // Hijack the Error.prepareStackTrace() function, which can be used to format the captured structuredStack.
-        var orig = Error.prepareStackTrace;
-        Error.prepareStackTrace = function(_, structuredStack){ return structuredStack; };
-        var err = new Error();
-
-        // Naming the anonymous function allows us to skip the top of the stack till customLGRLevel
-        Error.captureStackTrace(err, LGR.customLGRLevel);
-        var stack = err.stack;
-
-        // Don't forget to restore the hijacked function.
-        Error.prepareStackTrace = orig;
-        return stack;
-    }
-
-
     // Override ALL LEVELS ... to have timestamp
     Object.keys(NPMLOG.levels).forEach(function(k){
         // Name the anonymous function: Useful for capturing stack.
         LGR.prototype[k] = function customLGRLevel (){
-            arguments[0] = this._p(this.captureStack()[2]) + arguments[0]; // why stack[2]? think.
+            arguments[0] = this._p() + arguments[0];
             return this.NPMLOG[k].apply(this, arguments);
         };
     });
+
+    LGR.prototype['log'] = LGR.prototype['info'];
+    /*
+        by default we do not need to do stack trace
+    */
+    this.STACK_TRACE = false;
 }
 
-LGR.prototype.log = function(){
-    arguments[0] = this._p(this.captureStack()[2]) + arguments[0];
-    return this.NPMLOG['info'].apply(this, arguments);
+/*
+   Always nice to have __FUNC__, __FILE__, and __LINE__.
+   Referred from http://stackoverflow.com/questions/11386492/accessing-line-number-in-v8-javascript-chrome-node-js
+   Also read https://github.com/v8/v8/wiki/Stack-Trace-API
+*/
+
+function captureStack(){
+    // Hijack the Error.prepareStackTrace() function, which can be used to format the captured structuredStack.
+    var
+        orig = Error.prepareStackTrace,
+        err,
+        stack;
+
+    Error.prepareStackTrace = function(_, structuredStack){
+        return structuredStack;
+    };
+
+    err = new Error();
+    
+
+    // Naming the anonymous function allows us to skip the top of the stack till customLGRLevel
+    Error.captureStackTrace(err, LGR.customLGRLevel);
+    stack = err.stack;
+
+    // Don't forget to restore the hijacked function.
+    Error.prepareStackTrace = orig;
+    return stack;
 };
 
 /* Sets log format for a user */
 LGR.prototype.setLogFormat = function(val){
-    this.logFormat =  _.template(val);
+    this.logFormat  =  _.template(val);
+
+    /*
+        Now, no need to get stack trace every time ... use in template only if it is given
+    */
+    this.STACK_TRACE = TEMPLATE_PRINTS.some(function(templatePrint){
+        return (val.indexOf(templatePrint) > -1);
+    });
+    
 };
 
 /* returns log prefix */
-LGR.prototype._p = function(callSiteObj){
-    return this.logFormat({
-        "ram"       :  JSON.stringify(process.memoryUsage()),
-        "ts"        :  MOMENT().format("YYYY-MM-DD HH:mm:ss"),
-        "uptime"    : process.uptime(),
-        "pid"       : process.pid,
-        "count"     : this.count,
-        "__FUNC__"  : callSiteObj.getFunctionName() || '(anon)',
-        "__FILE__"  : callSiteObj.getFileName(),
-        "__LINE__"  : callSiteObj.getLineNumber(),
-        "__COLM__"  : callSiteObj.getColumnNumber(),
-    });
+LGR.prototype._p = function(){
+    var 
+        logFormatObject = {
+            "ram"       :  JSON.stringify(process.memoryUsage()),
+            "ts"        :  MOMENT().format("YYYY-MM-DD HH:mm:ss"),
+            "uptime"    : process.uptime(),
+            "pid"       : process.pid,
+            "count"     : this.count
+        },
+        callSiteObj;
+
+    if(this.STACK_TRACE){
+        callSiteObj = captureStack()[2];
+        _.set(logFormatObject,"__FUNC__",callSiteObj.getFunctionName() || '(anon)');
+        _.set(logFormatObject,"__FILE__",callSiteObj.getFileName());
+        _.set(logFormatObject,"__LINE__",callSiteObj.getLineNumber());
+        _.set(logFormatObject,"__COLM__",callSiteObj.getColumnNumber());
+    }
+    return this.logFormat(logFormatObject);
 };
 
 LGR.prototype.setLevel = function(level){
